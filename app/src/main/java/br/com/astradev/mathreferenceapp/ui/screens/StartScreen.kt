feb42815a786313f0e6d7aa4da67784d2290d5a8
MalcolnLMR <malcolnlucas@gmail.com>
2024-11-25 16:13:48 -0300

    add StartScreen

--- app/src/main/java/br/com/astradev/mathreferenceapp/ui/screens/StartScreen.kt
+++ app/src/main/java/br/com/astradev/mathreferenceapp/ui/screens/StartScreen.kt
@@ -1,32 +1,38 @@
 package br.com.astradev.mathreferenceapp.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import br.com.astradev.mathreferenceapp.R
 import br.com.astradev.mathreferenceapp.ui.components.AccentButton
 import br.com.astradev.mathreferenceapp.ui.components.MainBackground
-import br.com.astradev.mathreferenceapp.ui.components.NormalTextField
 import br.com.astradev.mathreferenceapp.ui.screens.objects.Routes
 import br.com.astradev.mathreferenceapp.ui.theme.BaseLight
+import br.com.astradev.mathreferenceapp.ui.theme.Black
 import br.com.astradev.mathreferenceapp.ui.theme.mathreferenceappTheme
 
 @Composable
-fun LoginScreen(navController: NavController){
-    val spacerSize = 30.dp
-    val confirmSpacerSize = 50.dp
+fun StartScreen(navController: NavController) {
+    val spacerSize = 40.dp
 
     MainBackground()
 
@@ -44,19 +50,49 @@ fun LoginScreen(navController: NavController){
                 .padding(20.dp)
         )
         {
-            NormalTextField(placeHolder = "email")
+            Text(
+                text = "Referências de Matemática",
+                modifier = Modifier
+                    .padding(horizontal = 3.dp)
+                    .widthIn(min = 50.dp),
+                color = Black,
+                fontSize = 20.sp
+            )
+
+            Spacer(modifier = Modifier.size(spacerSize - 20.dp))
+
+            Image(
+                painterResource(R.drawable.math),
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(70.dp)
+            )
+
             Spacer(modifier = Modifier.size(spacerSize))
-            NormalTextField(placeHolder = "senha")
-            Spacer(modifier = Modifier.size(confirmSpacerSize))
-            AccentButton("Entrar") { navController.navigate(Routes.HOME_SCREEN) }
+
+            AccentButton("Login",
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+            )
+            { navController.navigate(Routes.LOGIN_SCREEN) }
+            
+            Spacer(modifier = Modifier.size(spacerSize - 20.dp))
+
+            AccentButton("Cadastrar",
+                modifier = Modifier
+                    .width(200.dp)
+                    .height(50.dp)
+            )
+            { navController.navigate(Routes.REGISTER_SCREEN) }
         }
     }
 }
 
-@Composable
 @Preview(showBackground = true)
-fun LoginScreenPreview(){
+@Composable
+fun StartScreenPreview(){
     mathreferenceappTheme {
-        LoginScreen(rememberNavController())
+        StartScreen(rememberNavController())
     }
 }
